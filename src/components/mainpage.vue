d69ba394e3a9a39d09ded04d23ebd68b17d56978
chenhua <1204355436@qq.com>
2020-07-02 14:26:23 +0800

    init project

---
+++ src/components/mainpage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <h1>这个是顶级 layout</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.app {
+  width: 0 auto;
+  height: 100%;
+}
+</style>

c95fbd81ef8a732e761281d19789d93f1dafa22d
chenhua <1204355436@qq.com>
2020-07-09 10:23:15 +0800

    new

---
+++ src/test/01-02/vmodelandasync/checkbox.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>checkbox演示 vmodel</h1>
+    <div>
+      <template v-for="(item, idx) in model.options">
+        <input name="checkbox"
+               type="checkbox"
+               :key="idx"
+               :value="item.value"
+               v-model="item.checked" />{{item.label}}
+      </template>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'checkbox',
+  props: {
+    model: {
+      prop: 'checked',
+      event: 'change'
+    }
+  },
+  data () {
+    return {
+      model: {
+        options: [
+          {
+            label: "第一",
+            value: 'first',
+            checked: true
+          },
+          {
+            label: "第二",
+            value: 'second',
+            checked: false
+          },
+          {
+            label: "第三",
+            value: 'three',
+            checked: false
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/test/01-02/vmodelandasync/index.vue
+++ src/test/01-02/vmodelandasync/index.vue
@@ -1,40 +1,47 @@
 <template>
   <div>
-      <h1>vmodel演示</h1>
-      <div>
-          <input type="text" v-model="inputVal">
-      </div>
+    <h1>vmodel演示</h1>
+    <div>
+      <input type="text"
+             v-model="inputVal">
+    </div>
 
-      <div style="margin-top: 20px">
-          当前input 中的值为 {{inputVal}}
-      </div>
+    <div style="margin-top: 20px">
+      当前input 中的值为 {{inputVal}}
+    </div>
 
-      <h1>vmodel语法糖演示</h1>
-      <div>
-          <input type="text" :value="inputVal" @input="handleInput">
-      </div>
-
-      
+    <h1>vmodel语法糖演示</h1>
+    <div>
+      <input type="text"
+             :value="inputVal"
+             @input="handleInput">
+    </div>
+    <hr />
+    <checkbox v-model="selectItem"></checkbox>
+    <p>当前选中{{selectItem}}</p>
   </div>
 </template>
 
 <script>
+import checkbox from './checkbox'
 export default {
-    name: "vmodelandasync",
-    data () {
-        return {
-            inputVal: "333"
-        }
-    },
-    methods: {
-        handleInput (e) {
-            console.log(e)
-            this.inputVal = e.target.value
-        }
+  name: "vmodelandasync",
+  components: {
+    checkbox
+  },
+  data () {
+    return {
+      inputVal: "333",
+      selectItem: ""
     }
+  },
+  methods: {
+    handleInput (e) {
+      this.inputVal = e.target.value
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>

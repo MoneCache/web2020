a66ecc31ae969cb100c6d6b22dec49b3045e409f
chenhua <1204355436@qq.com>
2020-07-09 14:22:33 +0800

    modify

--- src/test/01-02/vmodelandasync/index.vue
+++ src/test/01-02/vmodelandasync/index.vue
@@ -19,19 +19,33 @@
     <hr />
     <checkbox v-model="selectItem"></checkbox>
     <p>当前选中{{selectItem}}</p>
+
+    <hr />
+    <h1>.sync演示</h1>
+    <sync :foo.sync="inputVal1"></sync>
+    <p>当前父组件中input的值为 {{inputVal1}}</p>
+
+    <!-- 1.总结 :
+    1.1. v-model 是 :value 和 @input 事件的语法糖
+    1.2. .sync 是 :foo.sync 和 @update:foo 事件的语法糖 (foo, 可以更改为任何字符串属性)
+    1.3. v-model 通常用在表单组件身上, 而, .sync修饰符,通常用在 自定义组件身上
+    1.4 v-model 控制能力在子组件是哪个, .sync 控制能力在父组件上 -->
   </div>
 </template>
 
 <script>
 import checkbox from './checkbox'
+import sync from './sync'
 export default {
   name: "vmodelandasync",
   components: {
-    checkbox
+    checkbox,
+    sync
   },
   data () {
     return {
       inputVal: "333",
+      inputVal1: "22",
       selectVal: [],
       selectItem: [
         {
---
+++ src/test/01-02/vmodelandasync/sync.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div>
+    <div>
+      <!--注意: 这里是表单组件, 一般不用 .sync修饰符, 这里只是做个范例 -->
+      <input type="text"
+             :value="foo"
+             @input="handleUpdate">
+    </div>
+    <div>
+      <p>当前input框的值为{{foo}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sync",
+  props: {
+    foo: {
+      type: String
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    handleUpdate (e) {
+      console.log(e)
+      this.$emit('update:foo', e.target.value)
+    }
+  }
+
+}
+</script>
+
+<style>
+</style>

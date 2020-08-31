981291a96adb607b016f844b8b4e72898c2d215b
chenhua <1204355436@qq.com>
2020-08-31 11:31:06 +0800

    fix project items

--- src/main.js
+++ src/main.js
@@ -2,15 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import store from './store/index'
-// 测试 创建的model
-// import create from './utils/create';
-import create from '@/test/01-01/notice/create';
 //测试自定义 router
 // import router from '@/test/01-02/selfRouter/index'
 import router from '@/router/router'
 
 Vue.config.productionTip = false;
-Vue.prototype.$create = create;
 
 new Vue({
   router,

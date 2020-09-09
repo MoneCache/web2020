e78e9b75b7418e902f020c1f3f56d43c3412d93e
chenhua <1204355436@qq.com>
2020-09-09 11:32:09 +0800

    fix bug

---
+++ vue/src/components/card/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-row>
+    <el-col :span="8"
+            v-for="(o, index) in 3"
+            :key="o">
+      <el-card>
+        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+             class="image">
+        <div class="full-width text-left ch-my-md ch-mt-md">
+          <p class="time">{{ currentDate }}</p>
+          <div class="bottom clearfix">
+            <h3 style="margin-top: 0"><a class="title">插画风格图鉴（下篇）：把鸡蛋放在哪个篮子里</a></h3>
+            <p class="content">我们将插画大体简单看成两类，一类是矢量插画，一类是用手绘板（笔）画出的插画，这两类插画最本质的区别在于实现成品的方式和流程，但有可能二者也能相互贯通融合。下面我会一一讲解自己所了解认识的插画风格。</p>
+          </div>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'blogcardindex',
+  data () {
+    return {
+      currentDate: new Date()
+    }
+  }
+}
+</script>
+
+<style>
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+.title {
+  text-decoration: none;
+  color: black;
+}
+.content {
+  line-height: 2em;
+  font-size: 12px;
+}
+</style>
--- vue/src/pages/fullpage/index.vue
+++ vue/src/pages/fullpage/index.vue
@@ -3,13 +3,19 @@
     <div class="back"
          :style="{'height': height + 'px'}"></div>
     <div class="back"
-         :style="{'height': height + 'px'}"></div>
+         :style="{'height': height + 'px'}">
+      <blog-card></blog-card>
+    </div>
   </div>
 </template>
 
 <script>
+import blogCard from '@/components/card/index'
 export default {
   name: 'fullpage',
+  components: {
+    blogCard
+  },
   data () {
     return {
       height: 0

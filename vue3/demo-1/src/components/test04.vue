f3f663578053f12073258634835a80f8d6890e40
chen <1204355436@qq.com>
2020-11-24 17:36:39 +0800

    add module

--- vue3/demo-1/src/components/test04.vue
+++ vue3/demo-1/src/components/test04.vue
@@ -7,20 +7,10 @@
 </template>
 
 <script lang="ts">
-import { ref } from 'vue'
+import { nowTime, getNowTime } from '../hooks/useNowTime'
 export default {
   name: 'test04',
   setup() {
-    const nowTime = ref("00:00:00")
-    const getNowTime = () => {
-      const now = new Date();
-      const hours = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
-      const minutes = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes()
-      const second = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds()
-      nowTime.value = `${hours}:${minutes}:${second}`
-      setInterval(getNowTime, 1000);
-    }
-
     return {
       nowTime,
       getNowTime

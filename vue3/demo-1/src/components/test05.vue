2a8939695a935735c8378a59454a0c4caa1aa38f
chen <1204355436@qq.com>
2020-11-25 13:53:39 +0800

    fix bug

--- vue3/demo-1/src/components/test05.vue
+++ vue3/demo-1/src/components/test05.vue
@@ -3,40 +3,38 @@
     <h1>以下是图片</h1>
     <h2>每3秒钟执行一次</h2>
     <div v-if="prloaded.value">
-    <img :src="prresult.value.message" alt="">
+      <img :src="prresult.value.message" alt="" />
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import fetch from '../hooks/useAxios'
+import { defineComponent, ref } from "vue";
+import fetch from "../hooks/useAxios";
 export default defineComponent({
-  name: 'test05',
+  name: "test05",
   setup() {
-    const prloading = ref(null)
-    const prloaded = ref(null)
-    const prresult = ref(null)
-    
+    const prloading = ref(null);
+    const prloaded = ref(null);
+    const prresult = ref(null);
 
     const func = () => {
-      setInterval(() => {
-      const {loading, loaded, result} = fetch("https://dog.ceo/api/breeds/image/random")
-      prloading.value = loading
-      prloaded.value = loaded
-      prresult.value = result
-    }, 5000);
-    }
-    func()
-  return {
-    prloading,
-    prloaded,
-    prresult
-  }
-  }
-})
+      const { loading, loaded, result } = fetch(
+        "https://dog.ceo/api/breeds/image/random"
+      );
+      prloading.value = loading;
+      prloaded.value = loaded;
+      prresult.value = result;
+      return func;
+    };
+    setInterval(func(), 5000);
+    return {
+      prloading,
+      prloaded,
+      prresult,
+    };
+  },
+});
 </script>
 
-<style>
-
-</style>
+<style></style>

bd6f887140510766d8c34e930c87cbde66835247
chen <1204355436@qq.com>
2020-11-27 14:42:46 +0800

    typescript 静态类型

--- typeScript/demo.ts
+++ typeScript/demo.ts
@@ -2,4 +2,45 @@ function getHello() {
   const web : string = "hello word"
   console.log(web)
 }
-getHello()
+// getHello()
+
+// number 类型
+const count: number = 1
+
+// string 类型
+const name1: string = 'ssss'
+
+//自定义静态类型
+
+interface Person {
+  name: string,
+  age: number,
+  action: () => string
+}
+
+const xioaming : Person = {
+  name: '小明',
+  age: 22,
+  action : () => {
+    return 'str'
+  }
+}
+
+// 对象 类型
+const obj : {
+  name: string,
+  age: number
+} = {
+  name: '笑话',
+  age: 22
+}
+console.log(obj.name)
+// 数组类型
+const xioahong : string[] = ['张大彪', '李二条']
+// 类类型
+class Person{}
+const dajiao: Person = new Person()
+// 函数类型
+const haha : () => string = () => {
+  return "大脚"
+}

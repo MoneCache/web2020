df3249499dded950b818a50bea6fdf03dd069d90
chen <1204355436@qq.com>
2020-12-10 14:52:05 +0800

    namespace 命名空间的使用

---
+++ typeScript/demo18/src/page.ts
@@ -0,0 +1,12 @@
+// 命名空间的使用
+
+namespace Home {
+  
+  export class Page {
+    constructor() {
+      new Components.Header();
+      new Components.Content();
+      new Components.Footer();
+    }
+  }
+}
